cdd7595b9cdeef0f78656635f4c4ed31278cce8f
Vishal Katti <katti.vishal@gmail.com>
2021-06-26 23:45:31 +0530

    Data Import

---
+++ 2021-06-22/Parks.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+# parks <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-22/parks.csv')
+# readr::write_csv(parks, "2021-06-22/parks.csv")
+
+
+parks <- read_csv("2021-06-22/parks.csv")
+
+
